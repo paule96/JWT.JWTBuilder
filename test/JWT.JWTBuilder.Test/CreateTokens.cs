bb626e757ecba5849d73c3965a202f79d8be5253
Paul Jeschke <paul-jeschke@outlook.com>
2017-10-31 17:30:53 +0100

    TEst for Build a token

--- test/JWT.JWTBuilder.Test/CreateTokens.cs
+++ test/JWT.JWTBuilder.Test/CreateTokens.cs
@@ -12,10 +12,8 @@ namespace JWT.JWTBuilder.Test
         public void CreateToken()
         {
             var token = new Builder()
-                .AddAlgorithm(new HMACSHA256Algorithm())
-                .AddSerializer(new JsonNetSerializer())
-                .AddUrlEncoder(new JwtBase64UrlEncoder())
-                .AddSecret("gsdhjfkhdfjklhjklgfsdhgfbsdgfvsdvfghjdjfgb")
+                .SetAlgorithm(new HMACSHA256Algorithm())
+                .SetSecret("gsdhjfkhdfjklhjklgfsdhgfbsdgfvsdvfghjdjfgb")
                 .Build();
             Assert.True(token.Length > 0 && token.Split('.').Length == 3);
         }
@@ -23,14 +21,43 @@ namespace JWT.JWTBuilder.Test
         [Fact]
         public void CreateTokenWithPayload()
         {
+            var testtime = DateTime.UtcNow.AddHours(5).ToString();
             var token = new Builder()
-                .AddAlgorithm(new HMACSHA256Algorithm())
-                .AddSerializer(new JsonNetSerializer())
-                .AddUrlEncoder(new JwtBase64UrlEncoder())
-                .AddSecret("gsdhjfkhdfjklhjklgfsdhgfbsdgfvsdvfghjdjfgb")
-                .AddClaim(PublicClaimsNames.ExpirationTime, DateTime.UtcNow.AddHours(5).ToString())
+                .SetAlgorithm(new HMACSHA256Algorithm())
+                .SetSecret("gsdhjfkhdfjklhjklgfsdhgfbsdgfvsdvfghjdjfgb")
+                .AddClaim(PublicClaimsNames.ExpirationTime, testtime)
                 .Build();
             Assert.True(token.Length > 0 && token.Split('.').Length == 3);
+            var decodedToken = System.Text.Encoding.UTF8.GetString(new JwtBase64UrlEncoder().Decode(token.Split('.')[1]));
+            Assert.True(decodedToken.Contains("exp") && decodedToken.Contains(testtime));
+        }
+        [Fact]
+        public void TryToCreateTokenWithoutInformaiton()
+        {
+            string token = null;
+            // Without any information
+            try
+            {
+                token = new Builder().Build();
+
+            }
+            catch (Exception)
+            {
+                Assert.True(true);
+            }
+            Assert.True(token == null);
+        }
+
+        [Fact]
+        public void TryToCreateTokenOnlyWithAlgorithm()
+        {
+            Assert.Throws<Exception>(() => new Builder().SetAlgorithm(new HMACSHA256Algorithm()).Build());
+        }
+
+        [Fact]
+        public void TryToCreateATokenOnlyWithSecret()
+        {
+            Assert.Throws<Exception>(() => new Builder().SetSecret("fjhsdghflghlk").Build());
         }
     }
 }
